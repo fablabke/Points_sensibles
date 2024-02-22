582ef5663567d344c9e1c363367f7c5993d69190
Juliette Vandermosten <158562798+juliettevandermosten@users.noreply.github.com>
2024-02-22 10:49:16 +0100

    Add files via upload

---
+++ Arduino/flex-test1.ino
@@ -0,0 +1,11 @@
+const int flexPin = A0;  // Broche analogique à laquelle est connecté le capteur de flexion
+
+void setup() {
+  Serial.begin(9600);  // Initialiser la communication série à 9600 bauds
+}
+
+void loop() {
+  int flexValue = analogRead(flexPin);  // Lire la valeur du capteur de flexion
+  Serial.println(flexValue);  // Envoyer la valeur via le port série
+  delay(100);  // Délai pour éviter une lecture trop rapide
+}
